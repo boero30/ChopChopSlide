8c50c9669db55fbca001620c162bc487d09c340d
Ghostman9515 <A01706603@tec.mx>
2022-11-24 16:25:20 -0600

    Asset borrados

---
+++ Assets/Scrpits_Jorge/ButtonLoadScenes.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonLoadScenes : MonoBehaviour
+{
+    public void LoadScene(string Scene)
+    {
+        SceneManager.LoadScene(Scene);
+    }
+
+}
